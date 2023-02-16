fcaece5c12287f2f244309d5daa5e7dee8c47159
Your Name <your.email@example.com>
2023-02-16 13:58:29 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  */
 
 
-int main (void)
+int main(void)
 {
-	   puts("\"Programming is like building a multilingual puzzle\n");
+	   puts("\"Programming is like building a multilingual puzzle");
 	   return 0;
 }
