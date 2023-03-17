e2219d470b3bc1e044ffc4b8ef3adfd5e3677da8
Your Name <your.email@example.com>
2023-03-17 11:06:24 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,6 +13,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int i;
 	void *new_ptr = NULL;
 
 	if (new_size == old_size)
@@ -39,7 +40,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 		new_size = old_size;
 
-	for (unsigned int i = 0; i < new_size; i++)
+	for (i = 0; i < new_size; i++)
 		((char *)new_ptr)[i] = ((char *)ptr)[i];
 
 	free(ptr);
